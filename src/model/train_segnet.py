36e99daee9c063671a0c55ffd1d51a8c94e5e1b0
okn-yu <mofuneko@okn-yunoMacBook-Air.local>
2019-09-01 17:54:04 +0900

    updated.

--- src/model/train_segnet.py
+++ src/model/train_segnet.py
@@ -42,14 +42,14 @@ def train():
             outputs = model(images.float())
             # print(outputs.shape)
             # loss = criterion(outputs, labels)
-            loss = cross_entropy2d(outputs, labels)
+            loss, input, target = cross_entropy2d(outputs, labels)
             train_loss += loss.item()
-            # train_acc += (outputs.max(1)[1] == labels).sum().item()
+            train_acc += (input.argmax(1) == target).sum().item()
             loss.backward()
             optimizer.step()
 
         avg_train_loss = train_loss / len(train_loader.dataset)
-        #avg_train_acc = train_acc / len(train_loader.dataset)
+        avg_train_acc = train_acc / (len(train_loader.dataset) * 360 * 480)
 
         model.eval()
         with torch.no_grad():
@@ -61,22 +61,22 @@ def train():
                 labels = torch.tensor(labels, dtype=torch.long, device=device)
 
                 outputs = model(images.float())
-                loss = cross_entropy2d(outputs, labels)
+                loss, input, target = cross_entropy2d(outputs, labels)
                 val_loss += loss.item()
-                #val_acc += (outputs.max(1)[1] == labels).sum().item()
+                val_acc += (input.argmax(1) == target).sum().item()
 
         avg_val_loss = val_loss / len(test_loader.dataset)
-        #avg_val_acc = val_acc / len(test_loader.dataset)
+        avg_val_acc = val_acc / (len(test_loader.dataset) * 360 * 480)
 
-        print('Epoch [{}/{}], Loss: {loss:.4f}, val_loss: {val_loss:.4f}'
-              .format(epoch + 1, NUM_EPOCHS, i + 1, loss=avg_train_loss, val_loss=avg_val_loss))
+        #print('Epoch [{}/{}], Loss: {loss:.4f}, val_loss: {val_loss:.4f}'
+        #      .format(epoch + 1, NUM_EPOCHS, i + 1, loss=avg_train_loss, val_loss=avg_val_loss))
 
-        #print('Epoch [{}/{}], Loss: {loss:.4f}, val_loss: {val_loss:.4f}, val_acc: {val_acc:.4f}'
-        #      .format(epoch + 1, NUM_EPOCHS, i + 1, loss=avg_train_loss, val_loss=avg_val_loss, val_acc=avg_val_acc))
+        print('Epoch [{}/{}], Loss: {loss:.4f}, val_loss: {val_loss:.4f}, val_acc: {val_acc:.4f}'
+              .format(epoch + 1, NUM_EPOCHS, i + 1, loss=avg_train_loss, val_loss=avg_val_loss, val_acc=avg_val_acc))
         train_loss_list.append(avg_train_loss)
-        #train_acc_list.append(avg_train_acc)
+        train_acc_list.append(avg_train_acc)
         val_loss_list.append(avg_val_loss)
-        #val_acc_list.append(avg_val_acc)
+        val_acc_list.append(avg_val_acc)
 
     plt.figure()
     plt.plot(range(NUM_EPOCHS), train_loss_list, color='blue', linestyle='-', label='train_loss')
@@ -87,14 +87,14 @@ def train():
     plt.title('Training and validation loss')
     plt.grid()
 
-    # plt.figure()
-    # plt.plot(range(NUM_EPOCHS), train_acc_list, color='blue', linestyle='-', label='train_acc')
-    # plt.plot(range(NUM_EPOCHS), val_acc_list, color='green', linestyle='--', label='val_acc')
-    # plt.legend()
-    # plt.xlabel('epoch')
-    # plt.ylabel('acc')
-    # plt.title('Training and validation accuracy')
-    # plt.grid()
+    plt.figure()
+    plt.plot(range(NUM_EPOCHS), train_acc_list, color='blue', linestyle='-', label='train_acc')
+    plt.plot(range(NUM_EPOCHS), val_acc_list, color='green', linestyle='--', label='val_acc')
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel('acc')
+    plt.title('Training and validation accuracy')
+    plt.grid()
 
     # torch.save(model.state_dict(), './trained_model')
 
@@ -105,15 +105,13 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
     n, c, h, w = input.size()
     nt, ht, wt = target.size()
 
-    # Handle inconsistent size between input and target
-    if h != ht and w != wt:  # upsample labels
-        input = F.interpolate(input, size=(ht, wt), mode="bilinear", align_corners=True)
+    # n, c, h, w -> n, h, c, w -> n, h, w, c
 
-    input = input.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    input = input.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c) # -> shape: (X, c)
     target = target.view(-1)
     loss = F.cross_entropy(
         input, target, weight=weight, size_average=size_average, ignore_index=250
     )
-    return loss
+    return loss, input, target
 
 train()
